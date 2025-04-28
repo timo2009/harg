193fa53e6823f16dcc8c29c088666476231aaf09
timo2009 <timo@tomsbox.de>
2025-04-28 21:20:12 +0200

    initial commit

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import os
+import random
+import string
+import requests
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def generate_random_id(length=14):
+    safe_characters = string.ascii_letters + string.digits + "#*!§$%&/()=?.,<>-_+`"
+    return ''.join(random.choice(safe_characters) for _ in range(length))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def check_network():
+    try:
+        response = requests.get('https://google.com', timeout=5)
+        if response.status_code == 200:
+            return True
+        else:
+            print("Network check failed. Retrying...")
+            return False
+    except requests.RequestException:
+        print("No network or unable to connect.")
+        return False
+
+
+def get_or_generate_id():
+    if os.path.exists("id"):
+        with open("id", "r") as f:
+            return f.read().strip()
+
+    print("ID does not exist. Creating a new one...")
+    new_id = generate_random_id()
+    with open("id", "w") as f:
+        f.write(new_id)
+
+    return new_id
+
+
+def main():
+    print("starting")
+    print("           ___     ____  _____")
+    print("|   |     /  \\    |  |  |")
+    print("|___|    /    \\   |--|  | ____")
+    print("|   |   /------\\  |  \\  |    |")
+    print("|   |  /        \\ |   \\ |____|")
+    print("________________________________")
+    print("HARG.14 by Timo Streich")
+    print("\nstart running")
+
+    check_network()
+
+    # ID abrufen oder generieren
+    id = get_or_generate_id()
+
+    # Deine ID ausgeben
+    print(f"Your public ID is: {id}")
+
+
+if __name__ == "__main__":
+    main()
